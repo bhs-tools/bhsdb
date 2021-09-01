73a62de9474dc00f71b55e391a361753b6caa45e
Jonah Matteson <57020076+insberr@users.noreply.github.com>
2021-09-01 16:41:33 +0000

    login?

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<script>
+import { ref } from 'vue'
+
+defineProps({
+  username: String
+})
+
+</script>
+
+<template>
+	<p>Hello, {{ username }}</p>
+	<input v-model="username">
+	<button type="button">Login</button>
+</template>
