fdb5963b13167e0ffcd0ddb9ad812ce98ed59db1
Wackery <zapcontact000@gmail.com>
2021-10-04 20:44:15 +0000

    my attempt to make the spinner better

--- components/spinner.js
+++ components/spinner.js
@@ -1,4 +1,4 @@
 import { Icon } from '@iconify/react';
 export default function Spinner() {
-    return <span className="animate animate-spin"><Icon icon="ei:spinner-3" inline={true} /></span>
+    return <span className="animate animate-spin w-1"><Icon icon="ei:spinner-3" inline={true} /></span>
 }
