22a4294ceac9883f18b5a0f3a10d7925a097deb2
Wackery <zapcontact000@gmail.com>
2021-10-04 16:31:42 +0000

    react sucks sometimes

--- components/helpers/studentName.js
+++ components/helpers/studentName.js
@@ -1,7 +1,7 @@
 import { get_student_info } from '../../lib/clientvue'
 import Spinner from '../spinner'
 function StudentName(props) {
-    var { student_info, isLoading, isError } = get_student_info()
+    var { student_info, isLoading, isError } = get_student_info(props.context) // todo: pull context from this and not parent
     if (isLoading) return <Spinner />
     if (isError) return <div>Error!</div>
     return (
--- components/pages/dashboard.js
+++ components/pages/dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import StudentName from '../helpers/studentName';
+import { UserContext } from '../BHSDB';
 export default class Dashboard extends React.Component {
+    static contextType = UserContext;
     render() {
         return (
             <div>
-                Hello <StudentName />!
+                Hello <StudentName context={this.context} />!
             </div>
         )
     }
