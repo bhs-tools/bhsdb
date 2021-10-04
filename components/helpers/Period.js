307ab6df84c43480604211a9c551260efecc1c7e
Wackery <zapcontact000@gmail.com>
2021-10-04 18:58:55 +0000

    make Period a component

--- components/helpers/Period.js
+++ components/helpers/Period.js
@@ -2,14 +2,20 @@ import { get_schedule } from '../../lib/clientvue'
 import Spinner from '../spinner'
 import { UserContext } from '../../lib/contexts';
 import React from 'react';
-export default class FirstPeriod extends React.Component {
+export default class Period extends React.Component {
     static contextType = UserContext;
     render() {
     var { schedule, isLoading, isError } = get_schedule(this.context)
     if (isLoading) return <Spinner />
     if (isError) return <div>Error!</div>
+    if (this.props.period === undefined) {
+        var period = 1
+    } else {
+        var period = this.props.period
+    }
+    var perioddata = schedule.content.ClassLists.ClassListing[period-1]
     return (
-        <span>{ schedule.content.ClassLists.ClassListing[0].CourseTitle }</span>
+        <span>{ perioddata.CourseTitle } in room { perioddata.RoomName }</span>
     )
     }
 }
--- components/pages/dashboard.js
+++ components/pages/dashboard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import StudentName from '../helpers/studentName';
-import FirstPeriod from '../helpers/FirstPeriod'
+import Period from '../helpers/Period'
 export default class Dashboard extends React.Component {
     
     render() {
         return (
             <div>
                 Hello <StudentName />!<br />
-                Your first period is <FirstPeriod />
+                Periods:
+                <ol>
+                    <li>1. <Period /></li>
+                    <li>2. <Period period={2} /></li>
+                    <li>3. <Period period={3}/></li>
+                    <li>4. <Period period={4}/></li>
+                    <li>5. <Period period={5}/></li>
+                </ol>
             </div>
         )
     }
