b8c00f0988708f52660337027345a6605c723e64
Wackery <zapcontact000@gmail.com>
2021-10-05 20:19:17 +0000

    use a skeleton for loading userinfo

--- components/userinfo.js
+++ components/userinfo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { get_student_info } from '../lib/clientvue'
-import { Avatar, Typography } from '@mui/material';
+import { Avatar, Typography, Skeleton } from '@mui/material';
 import { UserContext } from '../lib/contexts';
 export default class UserInfo extends React.Component {
     static contextType = UserContext;
@@ -8,11 +8,11 @@ export default class UserInfo extends React.Component {
         const { student_info, isLoading, isError } = get_student_info(this.context)
         if (this.props.loggedIn) {
             if ( isLoading || isError ) {
-                return ""
+                return <Skeleton variant="circular"><Avatar /></Skeleton>
             }
             return (
                     <React.Fragment>
-                        <Avatar alt="pfp" src={"data:image/png;base64,"+ student_info.Photo} />
+                        <Avatar alt="pfp" src={"data:image/png;base64,"+ student_info.Photo}/>
                         <Typography sx={{ marginLeft: "1em" }}>{ student_info.FormattedName }</Typography>
                     </React.Fragment>
                 )
