fd9ee412aa496637b01c186ba154ce9fea5e983a
Wackery <zapcontact000@gmail.com>
2021-10-06 18:57:48 +0000

    centering!!!

--- components/pages/login.js
+++ components/pages/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import { TextField, Typography } from '@mui/material';
+import { TextField, Typography, Grid } from '@mui/material';
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -17,12 +17,18 @@ export default class Login extends React.Component {
     }
     render() {
         return (
-            <div>
-                <Typography variant="h3">Login</Typography><br />
-                <TextField id="username" autoComplete="off" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange.bind(this)} /><br /><br />
-                <TextField type="password" id="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange.bind(this)} /><br /><br />
-                <Button variant="contained" onClick={this.handleLogin.bind(this)}> Login </Button> 
-            </div>
+            <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+                <Grid item>    
+                    <Typography variant="h3">Login</Typography><br />
+                </Grid> 
+                <Grid item>
+                    <TextField id="username" autoComplete="off" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange.bind(this)} /><br /><br />
+                    <TextField type="password" id="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange.bind(this)} /><br /><br />
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" onClick={this.handleLogin.bind(this)}> Login </Button> 
+                </Grid> 
+            </Grid>
         )
     }
 }
