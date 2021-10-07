5e9732a456b4fe1dce50eb5e2208b47570598df6
Wackery <zapcontact000@gmail.com>
2021-10-07 15:39:20 +0000

    Grades Page!
    also 100th commit

--- components/pages/grades.js
+++ components/pages/grades.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { GradeBar } from '../GradeBar'
+import GradeBar  from '../GradeBar'
 import { Grid, Paper, Box, styled } from '@mui/material'
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   }));
 
 export default class GradesPage extends React.Component {
@@ -14,22 +14,37 @@ export default class GradesPage extends React.Component {
         console.log("oh god gradespage")
         return (
             <Box>
-            <Grid container spacing={2}>
-                    <Grid item xs={1}>
-                        <Item><GradeBar period={1} /></Item>
+            <Grid container sx={{ textAlign: "center" }} spacing={2}>
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={1} />
                     </Grid>
-                    <Grid item xs={1}>
-                        <Item><GradeBar period={2} /></Item>
+                    <Grid item xs={1} />
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={2} />
                     </Grid>
-                    <Grid item xs={1}>
-                        <Item><GradeBar period={3} /></Item>
+                    <Grid item xs={1} />
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={3} />
                     </Grid>
-                    <Grid item xs={1}>
-                        <Item><GradeBar period={4} /></Item>
+                    <Grid item xs={1} />
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={4} />
                     </Grid>
-                    <Grid item xs={1}>
-                        <Item><GradeBar period={5} /></Item>
+                    <Grid item xs={1} />
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={5} />
                     </Grid>
+                    <Grid item xs={1} />
+                    <Grid item xs={1} />
+                    <Grid item xs={10}>
+                        <GradeBar period={6} />
+                    </Grid>
+                    <Grid item xs={1} />
                 </Grid>
             </Box>
         )
