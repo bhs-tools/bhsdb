52e0c960a0fb94f4e2bac2aa53c8eb7391283a11
webmsgr <zapcontact000@gmail.com>
2021-10-08 21:03:29 -0700

    Add link to source

--- components/pages/settings.js
+++ components/pages/settings.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Typography, FormGroup } from "@mui/material"
+import { Typography, FormGroup, Link } from "@mui/material"
 import ToggleSetting from "../settings/togglesetting"
 export default class SettingsPage extends React.Component {
     constructor(props) {
@@ -21,6 +21,7 @@ export default class SettingsPage extends React.Component {
                 <FormGroup>
                     <ToggleSetting checked={this.state.testsetting} handleChange={this.changeSettingToggle.bind(this)} label="Test setting (does nothing)" id="testsetting"/>
                     <ToggleSetting checked={this.state.testsetting2} handleChange={this.changeSettingToggle.bind(this)} label="Test setting 2 (does nothing)" id="testsetting2"/>
+                    <Link href="https://github.com/bhs-tools/bhsdb">Source code</Link>
                 </FormGroup>
             </Typography>
         )
