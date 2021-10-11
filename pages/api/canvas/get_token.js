ca2c5d2ff264ea9e31bb3828551370a05565e757
Wackery <zapcontact000@gmail.com>
2021-10-11 14:44:31 +0000

    add disabling of canvas, plus æ

---
+++ pages/api/canvas/get_token.js
@@ -0,0 +1,6 @@
+export default function get_token(req,res) {
+    if (process.env.ENABLE_CANVAS == "false" || process.env.ENABLE_CANVAS === undefined) {
+        res.status(200).json({ code: "DISABLED"})
+    }
+    // todo: actually get canvas token from creds in request
+}
